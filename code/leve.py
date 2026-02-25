daa72cf480f8234bd956140760f8e4f1d68e2c29
silva <2015adrianorodriguessilva@gmail.com>
2026-02-24 21:52:18 -0300

    Adding classes generated from StarUML

---
+++ code/leve.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Game import Game
+
+
+class Leve(Game):
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
---
+++ code/menu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Game import Game
+
+
+class Menu(Game):
+    def __init__(self):
+        self.window = None
+        self.Attribute1 = None
+
+    def run(self, ):
+        pass
